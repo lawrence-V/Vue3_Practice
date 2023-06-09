553a98147e52a9a78ca7a6c94a607829cd2236a3
lawrence-V <lawrencevaldez027@gmail.com>
2023-06-09 15:58:30 +0800

    setup global style

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -6,5 +6,17 @@ import { RouterView } from "vue-router";
   <RouterView />
 </template>
 
-<style scoped>
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300&family=Rubik:wght@300;400;700&display=swap");
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
 </style>
