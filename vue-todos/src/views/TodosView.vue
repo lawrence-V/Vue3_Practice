e2a4d794cd8668b113267ce806bbb7ad5ad13c82
lawrence-V <lawrencevaldez027@gmail.com>
2023-06-14 15:43:33 +0800

    add the delete function

--- vue-todos/src/views/TodosView.vue
+++ vue-todos/src/views/TodosView.vue
@@ -26,6 +26,10 @@ const toggleEditTodo = (todoPos) => {
 const updateTodo = (todoVal, todoPos) => {
   todoList.value[todoPos].todo = todoVal;
 };
+
+const deleteTodo = (todoId) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== todoId);
+};
 </script>
 
 <template>
@@ -41,6 +45,7 @@ const updateTodo = (todoVal, todoPos) => {
           @toggle-complete="toggleTodoComplete"
           @edit-todo="toggleEditTodo"
           @update-todo="updateTodo"
+          @delete-todo="deleteTodo"
           :key="index"
         />
       </ul>
